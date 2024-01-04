611e86c8e1730690741bf791c92fa9686c0d98bb
Ismail <ismail.baidar198@gmail.com>
2024-01-04 15:47:46 +0100

    [Add] adding Sidebar + Sidebar.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
+import { useState } from "react"
+
+import Sidebar from "./components/Sidebar"
+import "./assets/style/sidebar.css"
+import { Route, Routes } from "react-router-dom"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="app">
+      {/* routes */}
 
-    </>
+      <Sidebar />
+      <div className="content">
+        <Routes>
+          <Route path="/Experiences" element={<h1>Expe</h1>} />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
