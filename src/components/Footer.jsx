9c2385d5399831934dda07eb1dda3e26f844c334
SihamAlouani <sihamalouani2002@gmail.com>
2024-01-05 15:24:32 +0100

    hh

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ import {
   faLinkedin,
 } from "@fortawesome/free-brands-svg-icons"
 import image from "../assets/Images/CVLogo.jpeg"
-
+import '../assets/style/Footer.css'
 const Footer = () => {
   return (
     <footer>
