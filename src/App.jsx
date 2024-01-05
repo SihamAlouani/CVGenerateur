dc6a7898ae87ab54677e65d0526a78fbbb85c7a7
Ismail <ismail.baidar198@gmail.com>
2024-01-05 11:26:04 +0100

    [Fix] footer

--- src/App.jsx
+++ src/App.jsx
@@ -17,39 +17,39 @@ function App() {
   return (
     <div className="app">
       {/* routes */}
-      <Sidebar setPage={setPage} page={page} />
-      <div className="content">
-        <CustomRoute path="Information Personelle" page={page}>
-          <CollapseSection title="hello from something">
-            <div>
-              <h1>hello</h1>
-              <h1>world</h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Temporibus aspernatur praesentium nemo officia quo sed quis
-                nobis, atque tempore a omnis, eligendi quod possimus distinctio
-                ipsum? Tempora, odit optio? Illo!
-              </p>
-            </div>
-          </CollapseSection>
-        </CustomRoute>
-        <CustomRoute path="Education" page={page}>
-          <h1>this is Education </h1>
-        </CustomRoute>
-        <CustomRoute path="Experiences" page={page}>
-          <h1>this is Experiences </h1>
-        </CustomRoute>
-        <CustomRoute path="Compétances" page={page}>
-          <h1>this is Compétances </h1>
-        </CustomRoute>
-        <CustomRoute path="Langues" page={page}>
-          <LanguageSection />
-        </CustomRoute>
-        <CustomRoute path="Références" page={page}>
-          <h1>this is Références </h1>
-        </CustomRoute>
-
-        <Footer />
+      <div className="app-content">
+        <Sidebar setPage={setPage} page={page} />
+        <div className="content">
+          <CustomRoute path="Information Personelle" page={page}>
+            <CollapseSection title="hello from something">
+              <div>
+                <h1>hello</h1>
+                <h1>world</h1>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Temporibus aspernatur praesentium nemo officia quo sed quis
+                  nobis, atque tempore a omnis, eligendi quod possimus
+                  distinctio ipsum? Tempora, odit optio? Illo!
+                </p>
+              </div>
+            </CollapseSection>
+          </CustomRoute>
+          <CustomRoute path="Education" page={page}>
+            <h1>this is Education </h1>
+          </CustomRoute>
+          <CustomRoute path="Experiences" page={page}>
+            <h1>this is Experiences </h1>
+          </CustomRoute>
+          <CustomRoute path="Compétances" page={page}>
+            <h1>this is Compétances </h1>
+          </CustomRoute>
+          <CustomRoute path="Langues" page={page}>
+            <LanguageSection />
+          </CustomRoute>
+          <CustomRoute path="Références" page={page}>
+            <h1>this is Références </h1>
+          </CustomRoute>
+        </div>
       </div>
       <Footer />
     </div>
